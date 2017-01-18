d2064e0776c70058d66b323f28cc9bf6f61ac88d
huangjingcheng <Bravowhale@Bravowhale-PC>
2017-01-18 18:24:26 +0800

    websocket

---
+++ src/com/netty/websocket/handler/WebSocketServerHandler.java
@@ -0,0 +1,90 @@
+package com.netty.websocket.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.Date;
+
+import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
+import static io.netty.handler.codec.http.HttpUtil.setContentLength;
+
+/**
+ * Created by Bravowhale on 2017/1/18.
+ */
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+    public WebSocketServerHandler() {
+    }
+
+    private WebSocketServerHandshaker handshaker;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if(msg instanceof FullHttpRequest)
+            handleHttpRequest(ctx,(FullHttpRequest)msg);
+        else if (msg instanceof WebSocketFrame)
+            handleWebSocketFrame(ctx,(WebSocketFrame)msg);
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if(!req.decoderResult().isSuccess()
+                ||(!"websocket".equals(req.headers().get("Upgrade")))){
+            sendHttpResponse(ctx,req,new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        WebSocketServerHandshakerFactory wsFactory =new WebSocketServerHandshakerFactory("ws://localhost:8080/websocket",null,false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null){
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        }else {
+            handshaker.handshake(ctx.channel(),req);
+        }
+    }
+
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) {
+        if (res.status().code() != 200){
+            ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            setContentLength(res,res.content().readableBytes());
+        }
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!isKeepAlive(req) || res.status().code() != 200){
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame){
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        if (frame instanceof PingWebSocketFrame){
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        if(!(frame instanceof  TextWebSocketFrame)){
+            throw new UnsupportedOperationException(String .format("%s frame types not supported",frame.getClass().getName() ));
+        }
+        String  request = ((TextWebSocketFrame) frame).text();
+        ctx.channel().write(new TextWebSocketFrame(request
+        +",欢迎使用Netty WebSocket服务，现在时刻："
+        + new Date().toString()));
+    }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+
+}
---
+++ src/com/netty/websocket/server/WebSocketServer.java
@@ -0,0 +1,54 @@
+package com.netty.websocket.server;
+
+import com.netty.websocket.handler.WebSocketServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by Bravowhale on 2017/1/18.
+ */
+public class WebSocketServer {
+    public void run(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast("http-codec",
+                                    new HttpServerCodec());
+                            pipeline.addLast("aggregator",
+                                    new HttpObjectAggregator(65536));
+                            pipeline.addLast("http-chunked",
+                                    new ChunkedWriteHandler());
+                            pipeline.addLast("handler",
+                                    new WebSocketServerHandler());
+                        }
+                    });
+            Channel ch = b.bind(port).sync().channel();
+            System.out.println("web socket server started at port "+port+".");
+            System.out.println("Open your browser and navigate to http://localhost:"+port+"/");
+            ch.closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new WebSocketServer().run(8080);
+    }
+}
